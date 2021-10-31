9bcd6b8bfa882f8d9a2e2d2829966230746276ae
K-Karo <68198966+K-Karo@users.noreply.github.com>
2021-10-31 18:02:07 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,1 +1,60 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {BiRefresh} from "react-icons/bi"
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import {MainMenuBtn, InputTodo, Button, Select} from "./StyledComponents"
 
+const Form = ({inputTextTodo, setInputTextTodo, todos, setTodos, status, setStatus}) => {
+
+    const inputTextHandler = (e) => {
+        setInputTextTodo(e.target.value);
+    }
+    const submitTodoHandler = (e) => {   
+            e.preventDefault();
+            setTodos([
+            ...todos, {text: inputTextTodo, completed: false, id: Math.random()*1000}
+            ]);
+            setInputTextTodo("");         
+        }
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    }
+
+    const pass = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            setTodos([
+            ...todos, {text: inputTextTodo, completed: false, id: Math.random()*1000}
+            ]);
+            setInputTextTodo("");}
+         }
+
+    function refreshPage() {
+        window.location.reload();
+      }
+    
+    return(
+            <form>
+                    <MainMenuBtn onClick={ refreshPage }><BiRefresh Icon size={30}></BiRefresh></MainMenuBtn>
+                    <InputTodo 
+                        value={inputTextTodo} 
+                        onChange={inputTextHandler}
+                        onKeyPress={pass}>
+                    </InputTodo> 
+                    <Button 
+                        onClick={submitTodoHandler}>
+                        <FontAwesomeIcon icon={faPlusSquare} size='2x'></FontAwesomeIcon> 
+                    </Button>
+                    <div>   
+                        <Select 
+                            onChange={statusHandler}>
+                                <option>All</option>
+                                <option>Completed</option>
+                                <option>Uncompleted</option>
+                        </Select>
+                    </div>
+            </form>
+    );     
+}
+
+export default Form;
