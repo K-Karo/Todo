f1b967b5f86cea2c3c825abf683290c939180f7c
K-Karo <68198966+K-Karo@users.noreply.github.com>
2021-10-31 18:00:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,46 @@
+import React, {useState, useEffect} from 'react'
+import Form from './components/Form';
+import TodoList from "./components/TodoList"
 
+import './App.css';
+
+function App() {
+
+  const [todoActivator, setTodoActivator] = useState(false);
+  const [todoButtonHide, setTodoButtonHide] = useState(true);
+  const [inputTextTodo, setInputTextTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "Completed":
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case "Uncompleted":
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break
+      }
+    };   
+    filterHandler();
+  }, [status, todos]);
+
+  return (
+    <div className="App">
+      <Form 
+        todoActivator={todoActivator} setTodoActivator={setTodoActivator} 
+        inputTextTodo={inputTextTodo} setInputTextTodo={setInputTextTodo} 
+        todos={todos} setTodos={setTodos} 
+        status={status} setStatus={setStatus}/>
+      <TodoList 
+        todos={todos} setTodos={setTodos} filteredTodos={filteredTodos}/>
+    </div>
+  );
+}
+
+export default App;
